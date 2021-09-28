425fd48087e7ec2fcb6b171f875d0a1a6beafcf0
FoxCrush <63466872+FoxCrush@users.noreply.github.com>
2021-09-28 08:54:40 +0300

    window event listner switched to body

--- src/js/nav-panel.js
+++ src/js/nav-panel.js
@@ -2,17 +2,18 @@ const navRefs = {
     navMenuButton: document.querySelector(".nav-menu-button-container"),
     navMenuSlider: document.querySelector(".nav-slider-container"),
     navMenuCloseBtnEl: document.querySelector(".nav-menu-bar3"),
-    navMenuListEl: document.querySelector(".nav-list")
+    navMenuListEl: document.querySelector(".nav-list"),
+    bodyEl: document.querySelector("body")
 }
 
 function onNavMenuButtonClick(e) {
     e.preventDefault();
     if (!navRefs.navMenuSlider.classList.contains("tapped")) {
-            navRefs.navMenuButton.classList.add("tapped");
-    navRefs.navMenuSlider.classList.add("tapped");
-    setTimeout(() => {
-        console.log('listner added');
-        window.addEventListener('click', onOtherPlaceClick);
+        navRefs.navMenuButton.classList.add("tapped");
+        navRefs.navMenuSlider.classList.add("tapped");
+        setTimeout(() => {
+            console.log('listner added');
+            navRefs.bodyEl.addEventListener('click', onOtherPlaceClick);
     }, 200);
     }
 
@@ -20,7 +21,7 @@ function onNavMenuButtonClick(e) {
 function onOtherPlaceClick(e) {
     e.preventDefault();
     if (navRefs.navMenuButton.classList.contains("tapped") && e.target !== navRefs.navMenuListEl) {
-        window.removeEventListener('click', onOtherPlaceClick);
+        navRefs.bodyEl.removeEventListener('click', onOtherPlaceClick);
         console.log('listner removed');
         navRefs.navMenuButton.classList.remove("tapped");
         navRefs.navMenuSlider.classList.remove("tapped");
